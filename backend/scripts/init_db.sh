9e99376d4d01f60a08ba36e1c3b99fa3a1ecad21
Rolland ayonta <rollandayonta@gmail.com>
2025-12-27 11:19:28 +0100

    chore(backend): Add Docker PostgreSQL initialization to database setup script
    - Add environment variable defaults for database configuration (user, password, name, port)
    - Enable automatic database initialization with configurable parameters via environment variables

--- backend/scripts/init_db.sh
+++ backend/scripts/init_db.sh
@@ -17,3 +17,19 @@ if ! [-x "$(command -sqlx)"]; then
   echo >&2 " cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
   exit 1
 fi
+
+#check if custom informations of the database have been set, otherwise set default values
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+# Launch postgres using Docker with maximum number of connection set 1000
+docker run \
+-e POSTGRES_USER=${DB_USER} \
+-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+-e POSTGRES_DB=${DB_NAME} \
+-p "${DB_PORT}":5432 \
+-d postgres \
+postgres -N 1000
+
