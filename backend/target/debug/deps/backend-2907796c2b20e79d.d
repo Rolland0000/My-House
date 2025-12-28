d7a76c04a44b0179144a6d6ec6d3be45a7c041c9
Rolland ayonta <rollandayonta@gmail.com>
2025-12-28 16:48:40 +0100

    Update .gitignore

--- backend/target/debug/deps/backend-2907796c2b20e79d.d
+++
@@ -1,5 +0,0 @@
-D:\ME\Rust\My-House\backend\target\debug\deps\backend-2907796c2b20e79d.d: src\main.rs
-
-D:\ME\Rust\My-House\backend\target\debug\deps\libbackend-2907796c2b20e79d.rmeta: src\main.rs
-
-src\main.rs:
