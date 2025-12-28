ca1b61dab7cca2858e2900d07cbf7d5f94de6450
Rolland ayonta <rollandayonta@gmail.com>
2025-12-28 11:54:20 +0100

    chore(backend): Clean up build artifacts and update gitignore

--- backend/target/debug/deps/backend-b7c41b9028491353.d
+++
@@ -1,5 +0,0 @@
-D:\ME\Rust\My-House\backend\target\debug\deps\backend-b7c41b9028491353.d: src\main.rs
-
-D:\ME\Rust\My-House\backend\target\debug\deps\libbackend-b7c41b9028491353.rmeta: src\main.rs
-
-src\main.rs:
